a86b439c2c2bdd575ae549b85f82470b28d9e35c
Orion Hill <Orion123333@gmail.com>
2025-04-29 12:45:42 -0700

    Changed handling based on users previous watch time

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -1,32 +1,40 @@
 import React from "react";
 
-export default function VideoDetails({video, onBack, setRoute}){
+export default function VideoDetails({ video, onBack, setRoute, timeStamp }) {
 
-    let onPlay = function(){
+    let onPlay = function() {
         console.log("About to play the video!");
         setRoute("player");
     };
 
+    let buttons = [];
 
-    return(
-          <div style={{backgroundColor: 'black'}} className="video-details">
-            <button style={{color: 'white'}} onClick={onBack}>← Back to List</button>
-             <h1 style={{color: 'white'}}>{video.getVideoName()}</h1>
-             <img
-                    src={video.getVideoThumbnail()}
-                    alt={'Thumbnail for ' + video.getVideoName()}
-                    style={{ width:'50%', height:'50%' }}
-                />
-            <h2 style={{color: 'white'}}>{video.getVideoDescription()}</h2>
+    if (timeStamp == 0) {
+        buttons.push(<button class="text-xl" onClick={onPlay}>Play Video</button>);
+    }
+    else {
+        buttons.push(<button onClick={onPlay}>Resume/Continue</button>);
+    }
+    if (timeStamp != 0) {
+        buttons.push(<button>Start From Beginning</button>);
+    }
+    buttons.push(<button>Purchase {video.isFree() ? "Free!" : '$19.99'} </button>);
 
-            <div style={{color: 'white'}} className="options">
-                <button class="text-xl" onClick={onPlay}>Play Video</button>
-                <button>Purchase {video.isFree() ? "Free!" : '$19.99'} </button>
-                <button>Resume/Continue</button>
-                <button>Start From Beginning</button>
-                <button>Play Again</button>
+    return (
+        <div style={{ backgroundColor: 'black' }} className="video-details">
+            <button style={{ color: 'white' }} onClick={onBack}>← Back to List</button>
+            <h1 style={{ color: 'white' }}>{video.getVideoName()}</h1>
+            <img
+                src={video.getVideoThumbnail()}
+                alt={'Thumbnail for ' + video.getVideoName()}
+                style={{ width: '50%', height: '50%' }}
+            />
+            <h2 style={{ color: 'white' }}>{video.getVideoDescription()}</h2>
+            <div style={{ color: 'white' }} className="options">
+                {buttons}
             </div>
         </div>
-    );
-}
-{/* <li>{video.isFree() ? "Free!" : 'N/A'}</li> */}
+    )
+};
+
+{/* <li>{video.isFree() ? "Free!" : 'N/A'}</li> */ }
