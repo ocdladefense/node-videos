55614edf0945cb8953db728d58af719ff1537f3e
JesusMMorales <martinezja4@my.lanecc.edu>
2025-05-01 21:01:00 -0700

    edits

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -6,7 +6,6 @@ export default function VideoList({ video, videos, setSelectedVideo, setRoute })
 
     return (
         <div>
-            <h2 className="text-white bg-black" >Here is the list of videos!</h2>
             <ul className="video-list grid grid-cols-5 gap-7 text-white bg-black">
                 {videos.map((video, index) => (
                     <TitleComponent video={video} index={index} setRoute={setRoute} setSelectedVideo={setSelectedVideo} />
