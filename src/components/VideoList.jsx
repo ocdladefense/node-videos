1801cd1ab87319901ee53593565d6a0625290f00
JesusMMorales <martinezja4@my.lanecc.edu>
2025-05-07 20:13:18 -0700

    responsive

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -6,8 +6,8 @@ export default function VideoList({ video, videos, setSelectedVideo, setRoute, u
 
     return (
         <div className="p-8 bg-zinc-900 min-h-screen">
-            <h1 className="text-zinc-100 text-5xl font-bold mb-8">Welcome</h1>
-            <ul className="video-list grid phone:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-4 desktop:grid-cols-5 gap-8 text-zinc-100">
+            <h1 className="text-zinc-100 text-4xl font-bold pb-8 mb-8 text-left">Welcome</h1>
+            <ul className="video-list grid phone:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-4 desktop:grid-cols-5 gap-8">
                 {videos.map((video, index) => (
 
                     <TitleComponent video={video} index={index} setRoute={setRoute} setSelectedVideo={setSelectedVideo} user={user} />
