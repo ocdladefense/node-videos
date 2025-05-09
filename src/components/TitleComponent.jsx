c0182a2c371dc0979cd620718d00e9638cc26235
JesusMMorales <martinezja4@my.lanecc.edu>
2025-05-08 21:18:37 -0700

    emojis

--- src/components/TitleComponent.jsx
+++ src/components/TitleComponent.jsx
@@ -7,7 +7,7 @@ export default function TitleComponent({ video, index, setSelectedVideo, setRout
             onClick={() => { setRoute("details"); setSelectedVideo(video); }}
             style={{ cursor: 'pointer', marginBottom: '1rem' }}
         >
-            <div>
+            <div className="text-zinc-100">
                 {/* <strong className="text-2xl">{video.getVideoName()}</strong> <br /> */}
                 <img
                     src={video.getVideoThumbnail(video.getMaxResThumb())}
@@ -16,7 +16,21 @@ export default function TitleComponent({ video, index, setSelectedVideo, setRout
                 /> <br />
                 <strong className="text-2xl text-auto text-zinc-100">{video.getVideoName()}</strong> <br />
                 <ul>
-                    <li>{video.isFree() ? "Free!" : 'Paid'}</li>
+                    {/* <li>{video.isFree() ? "Free!" : 'Paid'}</li> */}
+                    <li className="flex items-center space-x-2">
+                        {video.isFree() ? (
+                            <>
+                                <span className="text-green-500">✔</span>
+                                <span className="text-green-700 font-medium">Free!</span>
+                            </>
+                        ) : (
+                            <>
+                                <span className="text-gray-400">🔒</span>
+                                <span className="text-gray-500">Paid</span>
+                            </>
+                        )}
+                    </li>
+
                 </ul>
             </div>
         </li>)
