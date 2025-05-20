b640b9e4df6ef09a00eb4e4171dd5f0e5d85bfc5
Gabe Gucker <guckerg.my.lanecc.edu>
2025-05-20 10:57:12 -0700

    fixed SalesforceApi references. Added example event controller

--- src/js/index.js
+++ src/js/index.js
@@ -47,10 +47,10 @@ console.log("user.get", user.get)
 window.ydc = new YoutubeDisplayController();
 
 
-const watchedVideosQuery = 'SELECT Timestamp__c FROM Watched_Video__c';
+const watchedVideosQuery = 'SELECT Name, UserId__c, ResourceId__c, Timestamp__c FROM Watched_Video__c';
 let sfrAPI = new SalesforceRestApi(SF_INSTANCE_URL, SF_ACCESS_TOKEN);
 let response = await sfrAPI.query(watchedVideosQuery);
-console.log("watched video query response", response);
+console.log("watched video query response", response.records);
 
 
 // without this I get an error at runtime.  babel 7 and preset env.
