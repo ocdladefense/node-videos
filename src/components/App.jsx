c20759132d2a75d9e8682496692d8941dcefdd07
José Bernal <jbernal.web.dev@gmail.com>
2025-05-23 09:39:50 -0700

    Prepare a root component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,28 @@
-import React from 'react';
+import Navbar from "@ocdla/global-components/src/Navbar";
+import Home from "./Home";
+import Footer from "@ocdla/global-components/src/Footer";
 
 
 // https://nextjs.org/docs/app/guides/migrating/from-create-react-app
+
+
+export default function App({ user }) {
+
+
+    return (
+        <div id="the-app-container">
+            <div class='fixed right-0 z-10 flex w-max gap-2 p-4 lg:left-0 lg:p-2'></div>
+            <header class="container mx-auto flex w-full flex-col top-of-page">
+                <Navbar />
+            </header>
+            <div class="container mx-auto">
+                <Home user={user} />
+            </div>
+            <Footer
+                showFacebook={true}
+                showTwitter={true}
+                useGoogleMapsIFrame={true}
+            />
+        </div>
+    );
+}
