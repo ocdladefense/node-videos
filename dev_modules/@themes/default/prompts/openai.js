d8f28d51806586c674e6c18ff567fb94b2c39edf
José Bernal <jbernal.web.dev@gmail.com>
2025-05-26 08:53:17 -0700

    Remove unused theme.

--- dev_modules/@themes/default/prompts/openai.js
+++
@@ -1,5 +0,0 @@
-/*
-
-A large group of conference attendees sit at circular dinner tables, their meals on the tables, in a modest ballroom.  The lights are dimmed and well-dressed female presenter with curly blond hair join a group of other presenters at a podium facing the dinner guests.  The podium has a placard that reads "OCDLA". The resulting image should be done in Analog Film style.
-
-*/
