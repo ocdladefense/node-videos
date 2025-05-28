2c1da27d1f94f7c16b086bbb530d787e7cbdb20e
José Bernal <jbernal.web.dev@gmail.com>
2025-05-28 09:08:37 -0700

    Build: remove unnecessary React imports.

--- src/components/VideoPlayerContainer.jsx
+++ src/components/VideoPlayerContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import '../css/videostyles.css';
 import VideoControlBar from './VideoControlBar';
 import VideoProgressBar from './VideoProgressBar';
