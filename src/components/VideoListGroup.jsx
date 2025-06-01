53cc0725a516820ba297b4caeeabac3cbe6bf6eb
José Bernal <jbernal.web.dev@gmail.com>
2025-06-01 10:00:58 -0700

    Bugfix: remove setRoute property, which is now superceded by routing.

--- src/components/VideoListGroup.jsx
+++ src/components/VideoListGroup.jsx
@@ -14,13 +14,12 @@ export default function VideoListGroup({ groups, user, setSelectedVideo }) {
 
                     return (
 
-
                         <ul className="video-list grid phone:grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-4 desktop:grid-cols-5 gap-8">
                             <li className='text-4xl text-zinc-100 grid col-span-full mt-10'>{key}</li>
                             <hr className='grid col-span-full text-zinc-100 -mt-5' />
                             {theGroup.map((video, index) => (
 
-                                <TitleComponent video={video} index={index} setRoute={setRoute} setSelectedVideo={setSelectedVideo} user={user} />
+                                <TitleComponent video={video} index={index} setSelectedVideo={setSelectedVideo} user={user} />
 
                             ))}
                         </ul>
