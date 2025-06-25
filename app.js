93ef3ca59e57f662f45e04d9f8e5fe089cca1578
José Bernal <jbernal.web.dev@gmail.com>
2025-06-24 21:55:38 -0700

    Use the client credentials flow.

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path'); // Import the path module
+const { access } = require('fs');
 const app = express();
 const port = process.env.PORT || 80;
+const SF_ACCESS_TOKEN = process.env.SF_ACCESS_TOKEN;
+const SF_INSTANCE_URL = process.env.SF_INSTANCE_URL;
 const SF_OAUTH_URL = process.env.SF_OAUTH_URL;
 const SF_CLIENT_ID = process.env.SF_CLIENT_ID;
 const SF_CLIENT_SECRET = process.env.SF_CLIENT_SECRET;
@@ -29,6 +32,66 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
+
+app.get("/introspect", async (req, res) => {
+
+
+    const data = new URLSearchParams({
+        token: SF_ACCESS_TOKEN,
+        client_id: SF_CLIENT_ID,
+        client_secret: SF_CLIENT_SECRET,
+        token_type_hint: "access_token"
+    });
+
+    console.log(data);
+
+    // Exchange authorization code for access token & id_token.
+    const resp = await fetch(SF_INSTANCE_URL + "/services/oauth2/introspect", {
+        method: "POST",
+        body: data,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+
+    const access_token_data = await resp.json();
+    console.log(access_token_data);
+
+
+});
+
+
+app.get("/connect", async (req, res) => {
+
+
+
+    const data = new URLSearchParams({
+        grant_type: "client_credentials",
+        client_id: SF_CLIENT_ID,
+        client_secret: SF_CLIENT_SECRET
+    });
+
+    console.log(data);
+
+    // Exchange authorization code for access token & id_token.
+    const response = await fetch("https://ocdla--ocdpartial.sandbox.my.salesforce.com/services/oauth2/token", {
+        method: "POST",
+        body: data,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
+
+    console.log("Receiving response...");
+    const token = await response.json();
+    console.log(token);
+
+    res.json(token);
+});
+
+
 
 
 app.get("/login", (req, res) => {
